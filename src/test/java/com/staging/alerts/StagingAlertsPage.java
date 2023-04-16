069de949b32753653ae7749f04346a5ab23474c7
Daniel Nicholson <danielnicholson@Daniels-Air.home>
2023-04-15 19:55:55 -0500

    adding staging for alerts

--- src/test/java/com/staging/alerts/StagingAlertsPage.java
+++ src/test/java/com/staging/alerts/StagingAlertsPage.java
@@ -1,8 +1,12 @@
 package com.staging.alerts;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 
 public class StagingAlertsPage {
 
@@ -10,5 +14,45 @@ public class StagingAlertsPage {
 
         WebDriverManager.firefoxdriver().setup();
         WebDriver driver = new FirefoxDriver();
+
+        driver.get("https://testpages.herokuapp.com/styled/alerts/alert-test.html");
+        driver.manage().window().maximize();
+
+        // Verify alert box
+
+        driver.findElement(By.id("alertexamples")).click();
+
+        Alert alert = driver.switchTo().alert();
+
+        String actualAlertText = alert.getText();
+        String expectedAlertText = "I am an alert box!";
+
+        Assert.assertEquals(actualAlertText, expectedAlertText, "Actual text does not match expected text");
+
+        alert.accept();
+
+        // Verify confirm box
+
+        driver.findElement(By.id("confirmexample")).click();
+
+        String actualDismissText = alert.getText();
+        String expectedDismissText = "You clicked Cancel, confirm returned ";
+        System.out.println(actualDismissText);
+
+        alert.dismiss();
+
+        driver.findElement(By.id("confirmexample")).click();
+
+        String actualAcceptText = alert.getText();
+        String expectedAcceptText = "You clicked OK, confirm returned ";
+        System.out.println(actualAcceptText);
+
+        alert.accept();
+
+        // Verify prompt box
+
+        // Teardown
+
+//        driver.quit();
     }
 }
